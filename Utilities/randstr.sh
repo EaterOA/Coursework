d80c9b32847a219cc7ca6bf2c8c00308b2ee9856
Vincent Wong <duperduper@ucla.edu>
2016-03-19 13:41:32 -0700

    Change randstr.sh default options

--- Utilities/randstr.sh
+++ Utilities/randstr.sh
@@ -2,37 +2,42 @@
 
 # randstr.sh
 #
-# Uses /dev/urandom to generates a string of random printable characters, with
+# Uses /dev/urandom to generate a string of random printable characters, with
 # a default length of 16. Useful for generating random passwords.
 
 err_exit() {
     cat >&2 <<EOF
-Usage: randstr.sh [-an] [-c number_of_characters]
+Usage: randstr.sh [-an] [number_of_characters]
+
+Randstr provides a simple way to construct a random sequence of characters.
+Perfect for passwords. The number of characters can be adjusted with an integer
+argument. If unspecified, the default is 16.
 
 Options:
     -a      generate alpha characters
     -n      generate numeric characters
-    -c      specify the number of characters to generate (default 16)
+    -s      generate special characters
 EOF
     exit 1
 }
 
 numchars="16"
-alpha=""
-numeric=""
-printable="[:print:]"
-while getopts ":anc:" opt; do
+filtered=false
+fields="[:alpha:][:digit:]"
+while getopts ":ans" opt; do
+    if [ "$filtered" = false ]; then
+        fields=""
+        filtered=true
+    fi
     case $opt in
         n)
-            printable=""
-            numeric="[:digit:]"
+            fields="${fields}[:digit:]"
             ;;
         a)
-            printable=""
-            alpha="[:alpha:]"
+            fields="${fields}[:alpha:]"
             ;;
-        c)
-            numchars="$OPTARG"
+        s)
+            fields="${fields}[:punct:]"
             ;;
         \?)
             err_exit
@@ -41,4 +46,13 @@ while getopts ":anc:" opt; do
 done
 shift $(($OPTIND-1))
 
-tr -cd "${printable}${alpha}${numeric}" < /dev/urandom | head -c "$numchars" ; echo
+# numchars argument must be an integer
+if [ ! -z "$1" ]; then
+    re='^[0-9]+$'
+    if ! [[ "$1" =~ $re ]]; then
+        err_exit
+    fi
+    numchars="$1"
+fi
+
+tr -cd "$fields" < /dev/urandom | head -c "$numchars" ; echo
